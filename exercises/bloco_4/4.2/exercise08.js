8166a8daa2f70c23154edd43a0b953bf7be0fcae
Micaela Alves <micaela.alves93@gmail.com>
2020-11-26 19:34:01 -0300

    Exercicio 8 e 9

--- exercises/bloco_4/4.2/exercise08.js
+++ exercises/bloco_4/4.2/exercise08.js
@@ -1,3 +1,9 @@
 //Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [];
+
+for(var index = 1; index <= 25; index = index + 1){
+    numbers.push(index);
+}
+
+console.log(numbers);
--- exercises/bloco_4/4.2/exercise09.js
+++ exercises/bloco_4/4.2/exercise09.js
@@ -1,3 +1,9 @@
 // Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [];
+
+for(var index = 1; index <= 25; index = index + 1){
+    numbers.push(index);
+    console.log(index / 2);
+}
+
