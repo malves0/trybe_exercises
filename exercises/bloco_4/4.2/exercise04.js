52bcc9be4c6e74881bf8fa6d39523d10ee90e711
Micaela Alves <micaela.alves93@gmail.com>
2020-11-26 17:59:35 -0300

    Exercicio 04

--- exercises/bloco_4/4.2/exercise04.js
+++ exercises/bloco_4/4.2/exercise04.js
@@ -2,3 +2,16 @@
 //Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sumnumber = 0; averagenumbers = 0;
+
+for(let index = 0; index < numbers.length; index = index + 1){
+    sumnumber += numbers[index];
+}
+averagenumbers = sumnumber / numbers.length;
+
+if (averagenumbers > 20){
+    console.log('Valor maior que 20');
+}else{
+    console.log('Valor menor ou igual a 20');
+}
+console.log(averagenumbers);
