3a7a36e09f55f9b6bd3dfc8b9a9958e87a28bba7
Micaela Alves <micaela.alves93@gmail.com>
2020-12-17 23:13:50 -0300

    bloco 5

--- exercises/bloco_5/5.2/main_at01.js
+++ exercises/bloco_5/5.2/main_at01.js
@@ -22,5 +22,34 @@ console.log(atencao);
 let terceiroFilho = document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
 console.log(terceiroFilho);
 //8
-terceiroFilho = document.querySelector('#pai').childNodes[5]
-console.log(terceiroFilho)
+terceiroFilho = document.querySelector('#pai').childNodes[5];
+console.log(terceiroFilho);
+console.log('Parte II')
+//Parte II
+//1
+let divNova = document.createElement('div');
+let local = document.querySelector('#elementoOndeVoceEsta').parentNode;
+local.appendChild(divNova);
+//2
+local = document.querySelector('#elementoOndeVoceEsta');
+local.appendChild(divNova);
+//3
+local = document.querySelector('#primeiroFilhoDoFilho');
+local.appendChild(divNova).id = 'filhoDoPrimeiroFilhoDoFilho';
+//4
+local = document.querySelector("#filhoDoPrimeiroFilhoDoFilho")
+local = local.parentNode.parentNode.parentNode.childNodes[5]
+console.log(local)
+// Parte III
+//1
+
+var elemento = document.getElementById("paiDoPai");
+while (elemento.firstChild) {
+    let id = elemento.firstChild.id
+    console.log(id)
+   // if (id !== 'pai'){
+        elemento.removeChild(elemento.firstChild);
+  // }
+}
+
+/** Remova todos os elementos da página, menos `pai` , `elementoOndeVoceEsta` e `primeiroFilhoDoFilho` .*/
