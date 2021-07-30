4c0c19237e76189e71b0fbf1f042dd384745cb5d
Micaela Alves <micaela.alves93@gmail.com>
2021-07-30 16:00:26 -0300

    model book

--- exercises/back-end/bloco_27/27.1/model-example/index.js
+++ exercises/back-end/bloco_27/27.1/model-example/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
 const Author = require('./models/Author');
+const Book = require('./models/Book');
 
 const app = express();
 
 app.get('/authors', async (_req, res) => { 
     const authors = await Author.getAll();
     res.status(200).json(authors);
+});
+
+app.get('/books', async (_req, res) => {
+    const books = await Book.getAll();
+    res.status(200).json(books);
 });
 
 const PORT = process.env.PORT || 3000;
