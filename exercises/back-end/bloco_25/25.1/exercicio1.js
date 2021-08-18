6c76f9a27e97e99f97471197315c484d2828b6a2
Micaela Alves <micaela.alves93@gmail.com>
2021-08-18 19:46:48 -0300

    Exercicios 1 a 8

---
+++ exercises/back-end/bloco_25/25.1/exercicio1.js
@@ -0,0 +1,8 @@
+db.clientes.aggregate([
+  { 
+    $match: 
+      {
+        sexo: "MASCULINO"
+      }
+  }
+]);
---
+++ exercises/back-end/bloco_25/25.1/exercicio6.js
@@ -0,0 +1,11 @@
+db.clientes.aggregate([
+  {
+    $group: {
+      _id: {
+        sexo: "$sexo",
+        uf: "$endereco.uf"
+      },
+      total: { $sum: 1 }
+    }
+  }
+]);
