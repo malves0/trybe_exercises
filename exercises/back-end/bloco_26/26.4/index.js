d7edddbc2016750891df1587b69edf29f50ab452
Micaela Alves <micaela.alves93@gmail.com>
2021-08-27 18:53:11 -0300

    add bodyParser

--- exercises/back-end/bloco_26/26.4/index.js
+++ exercises/back-end/bloco_26/26.4/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
 // 1
 app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong' })
+});
+
+// 2
+app.post('/hello', (_req, res) => {
+
 });
 
 app.listen(3000, function () {
