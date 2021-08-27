06aef82cf3c41e4f1b3d6bcfd05377194b9c6946
Micaela Alves <micaela.alves93@gmail.com>
2021-08-27 19:26:56 -0300

    Exercicio 4

--- exercises/back-end/bloco_26/26.4/index.js
+++ exercises/back-end/bloco_26/26.4/index.js
@@ -23,6 +23,12 @@ app.post('/greetings', (req, res) => {
   res.status(401).json({ 'message': 'Unauthorized'});
 });
 
+// 4
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.status(200).json({ 'message': `Seu nome é ${name} e você tem ${age} anos de idade`});
+});
+
 app.listen(3000, function () {
   console.log('App listening on port 3000!!')
 });
