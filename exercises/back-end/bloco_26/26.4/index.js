969d5717b3a70811f79490a9305a3b7265871199
Micaela Alves <micaela.alves93@gmail.com>
2021-08-27 18:49:17 -0300

    exercicio 1

--- exercises/back-end/bloco_26/26.4/index.js
+++ exercises/back-end/bloco_26/26.4/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// 1
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'pong' })
+});
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!!')
