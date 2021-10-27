71298c8914b31e8dbb12d95b48fb15584f2ad18e
Micaela Alves <micaela.alves93@gmail.com>
2021-10-27 20:18:25 -0300

    criando um novo escritor

--- exercises/back-end/bloco_30/30.1/mvc-example/controllers/authorController.js
+++ exercises/back-end/bloco_30/30.1/mvc-example/controllers/authorController.js
@@ -15,7 +15,24 @@ const showAuthor = async (req, res) => {
   res.status(200).render('authors/show', { author });
 };
 
+const newAuthor = (req, res) => {
+  res.render('authors/new', { message: null });
+};
+
+const createAuthor = async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+    return res.render('authors/new', { message: 'Dados inválidos' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+  res.redirect('authors');
+};
+
 module.exports = {
   listAuthors,
   showAuthor,
+  newAuthor,
+  createAuthor,
 }
--- exercises/back-end/bloco_30/30.1/mvc-example/index.js
+++ exercises/back-end/bloco_30/30.1/mvc-example/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 require('dotenv').config()
+const bodyParser = require('body-parser');
 
 const authorController = require('./controllers/authorController');
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.set('views', './views')
 
 app.get('/authors', authorController.listAuthors);
+app.get('/authors/new', authorController.newAuthor);
 app.get('/authors/:id', authorController.showAuthor);
 
+app.post('/authors', authorController.createAuthor);
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000;
